fc85db4e881b75ccbac34b6f23115c9eda8b1b16
Haroon <mharoon2595@gmail.com>
2023-09-27 18:36:00 +0530

    Second commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,19 @@ import style from './Hero.module.css'
 const Hero = () => {
   return (
     <div className={style.hero}>
-       <div className={style.orange}> </div>
+       <div className={style.textContainer}>
+
+        <div className={style.text1}>
+          <p>You don't have to </p>
+          <p><strong>Fight them Alone.</strong></p>
+          </div>
+
+          <div className={style.text2}>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit blandit, curabitur sodales conubia ut inceptos faucibus himenaeos tortor eget, hac massa gravida arcu interdum proin curae.</p>
+          </div>
+
+          <input className={style.text3}></input>
+       </div>
        <div className={style.heroImage}>
          <HeroLawyer/>
        </div>
